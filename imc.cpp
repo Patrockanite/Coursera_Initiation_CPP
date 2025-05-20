82dfac70f07d81d36acd128965da4eb921fe4038
Patrockanite <patrickannoot@orange.fr>
2025-05-20 15:03:29 +0200

    Modification de imc.cpp

--- imc.cpp
+++ imc.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
+    double poids(74.5);
+    double taille(1.75);
+
+    //Entrées des valeurs
+    cout << "Entrez un poids en Kg : ";
+    cin >> poids;
+    cout << "Entrez une taille en mètre : ";
+    cin >> taille;
+    double imc(poids / (taille*taille));
+
+    cout<< "Pour " << poids << " Kg et " << taille
+    << " m, l'IMC est de " << imc << endl;
 return 0;
 }
+
