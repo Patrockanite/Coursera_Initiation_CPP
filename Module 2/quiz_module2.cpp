161f5b468278ccfc52dd94652b887fa8b479dc17
Patrockanite <patrickannoot@orange.fr>
2025-05-26 13:21:54 +0200

    Ajout quiz module 2

---
+++ Module 2/quiz_module2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace::std;
+
+int main(){
+
+    int nb_etudiants(300);
+    int nb_places(600);
+    if(nb_etudiants * 2 !<  nb_places){ // fausse
+        cout<<"Ca ne marche pas"<<endl;
+    }
+    /* code */
+    return 0;
+}
